e4f7b3f7b559a921eb4ca56a6a2429aa321b5f68
José Ignacio Castro Martínez <JCastro@planexware.com>
2023-07-21 12:25:58 -0300

    feature: agrego logica a las tarjetas

--- src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms'
 
 @Component({
   selector: 'app-tarjeta-credito',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./tarjeta-credito.component.css']
 })
 export class TarjetaCreditoComponent {
+
+  form!: FormGroup;
+
+  listTarjetas: any[] = [
+    { nombre: "Juan Perez", numeroTarjeta: "123132131", fechaExpiracion: "11/23", cvv: "122" },
+    { nombre: "Amanda Rodriguez", numeroTarjeta: "960843214", fechaExpiracion: "12/28", cvv: "221" },
+    {nombre: "Jose ignacio", numeroTarjeta: "123456789", fechaExpiracion: "12/45", cvv: "113"}
+  ];
+  
+  
+  constructor(private fb: FormBuilder){
+    this.form = this.fb.group({
+      titular: [''],
+      numeroTarjeta: [''],
+      fechaExpiracion: [''],
+      cvv: ['']
+    })
+  }
+
+  validarTarjeta(numeroTarjeta: string){
+    return numeroTarjeta.length < 16 && numeroTarjeta.length < 0; 
+  }
+
+  cargarListaTarjetas(){
+    
+    return this.listTarjetas;
+  }
+
+  ngOnInit(){}
 
 }
